16fb76e34109894cbe17d6fb85b596059a29a8ec
Jhonier <Jhonierstephan16@gmail.com>
2025-03-20 17:06:33 -0500

    Add: config safeArea

--- App.tsx
+++ App.tsx
@@ -1,16 +1,23 @@
-import { PortalProvider, TamaguiProvider } from "tamagui";
-import tamaguiConfig from "./tamagui.config";
-import StackNavigator from "./navigation/StackNavigator";
-import { NavigationContainer } from "@react-navigation/native";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { PortalProvider, TamaguiProvider } from 'tamagui';
+import tamaguiConfig from './tamagui.config';
+import StackNavigator from './navigation/StackNavigator';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <TamaguiProvider config={tamaguiConfig}>
-      <PortalProvider shouldAddRootHost>
-        <NavigationContainer>
-          <StackNavigator />
-        </NavigationContainer>
-      </PortalProvider>
-    </TamaguiProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <TamaguiProvider config={tamaguiConfig}>
+          <PortalProvider shouldAddRootHost>
+            <NavigationContainer>
+              <StackNavigator />
+            </NavigationContainer>
+          </PortalProvider>
+        </TamaguiProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
+
