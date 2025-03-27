a62a3c3a58303c8872f67b89a7077fcde556ded2
Jhonier <Jhonierstephan16@gmail.com>
2025-03-26 23:05:13 -0500

    Refactor: Agenda view

--- components/Agenda.tsx
+++ components/Agenda.tsx
@@ -1,49 +1,79 @@
-import React from "react";
-import { Text, View } from "tamagui";
+import React, { useState } from "react";
+import { Card, Paragraph, ScrollView, Text, View, XStack, YStack } from "tamagui";
 import { Agenda as RNCAgenda } from "react-native-calendars";
 import { AgendaItem } from "../types";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { useThemeStore } from "../store/useThemeStore";
 
 interface AgendaProps {
   items: Record<string, AgendaItem[]>;
 }
 
 export const Agenda: React.FC<AgendaProps> = ({ items }) => {
+  const [selectedDate, setSelectedDate] = useState<string | null>(null);
+  const { themeStyles } = useThemeStore();
+
   return (
-    <RNCAgenda
-      items={items}
-      renderItem={(item: AgendaItem, isFirst: boolean) => (
-        <View
-          style={{
-            padding: 12,
-            backgroundColor: "#fff",
-            marginVertical: 8,
-            borderRadius: 12,
-            shadowColor: "#000",
-            shadowOpacity: 0.1,
-            shadowRadius: 4,
-            elevation: 2,
-          }}
-        >
-          <Text fontWeight="bold" fontSize={16} color="#333">
-            📅 {item.name}
-          </Text>
-          <Text fontSize={14} color="#666">
-            <MaterialCommunityIcons
-              name="clock-outline"
-              size={20}
-              color="#666"
-            />{" "}
-            {item.start} - {item.end}
-          </Text>
-        </View>
-      )}
-      theme={{
-        selectedDayBackgroundColor: "#007AFF",
-        todayTextColor: "#007AFF",
-        agendaTodayColor: "#007AFF",
-        dotColor: "#007AFF",
-      }}
-    />
+    <View style={{ flex: 1, backgroundColor: "#F8F9FA" }}>
+      <RNCAgenda
+        items={items}
+        onDayPress={(day: any) => setSelectedDate(day.dateString)}
+        renderItem={(item: AgendaItem) => (
+          <Card
+            bordered
+            padding="$4"
+            style={{
+              backgroundColor: "#fff",
+              borderRadius: 12,
+              shadowColor: "#000",
+              shadowOpacity: 0.1,
+              shadowRadius: 4,
+              elevation: 3,
+              marginBottom: 10,
+              marginTop: 20,
+            }}
+          >
+            <YStack gap="$2">
+              <XStack alignItems="center" gap={10}>
+                <MaterialCommunityIcons
+                  name="calendar-check"
+                  size={22}
+                  color={themeStyles.primary}
+                />
+                <Text fontWeight="bold" fontSize={16}>
+                  {item.name}
+                </Text>
+              </XStack>
+              <XStack alignItems="center" gap={8}>
+                <Ionicons
+                  name="book-outline"
+                  size={20}
+                  color={themeStyles.primary}
+                />
+                <Paragraph size="$4" fontWeight="600">
+                  Materia: {/* {item.subject} */} Programacion
+                </Paragraph>
+              </XStack>
+              <XStack alignItems="center" gap={8}>
+                <Ionicons
+                  name="time-outline"
+                  size={22}
+                  color={themeStyles.primary}
+                />
+                <Paragraph size={"$4"} color={themeStyles.text}>
+                  {item.start} - {item.end}
+                </Paragraph>
+              </XStack>
+            </YStack>
+          </Card>
+        )}
+        theme={{
+          selectedDayBackgroundColor: "#007AFF",
+          todayTextColor: "#007AFF",
+          agendaTodayColor: "#007AFF",
+          dotColor: "#007AFF",
+        }}
+      />
+    </View>
   );
 };
